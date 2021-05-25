abf052a48a7c7382f493c4c30dae29704d4860ec
hongbi-lee <foxgirin950@gmail.com>
2021-05-26 03:16:50 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
-function Food(){
-    return <h1>I like Potato</h1>;
-}
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        movies: [],
+    };
 
-function App() {
-  return (
-      <div>
-        <h1>Hello</h1>
-          <Food fav="kimchi"/>
-      </div>
-  );
+    getMovies = async () => {
+        const {
+            data : {
+                data : { movies }
+            }
+        } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+        this.setState({ movies: movies });
+    }
+
+    componentDidMount() {
+        // 영화 데이터 로딩
+        this.getMovies();
+    }
+
+    render() {
+        const { isLoading } = this.state;
+        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+    }
 }
 
 export default App;
