8da5a3bf8faf7889f1c2d2a9398f9dfb9abefe5d
hongbi-lee <foxgirin950@gmail.com>
2021-11-01 19:09:04 +0900

    p.100까지 공부 및 실습 완료

---
+++ src/test/java/com/spbt2/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.spbt2.book.springboot.web.dto;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void lombokAbleTest() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+}
