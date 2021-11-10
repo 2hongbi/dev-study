69b670029426e98e97635aa4d5dcf13758317d41
hongbi-lee <foxgirin950@gmail.com>
2021-11-10 15:50:01 +0900

    done with p.150

--- src/main/java/com/spbt2/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/spbt2/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.spbt2.book.springboot.service.posts;
 
 import com.spbt2.book.springboot.domain.posts.Posts;
 import com.spbt2.book.springboot.domain.posts.PostsRepository;
+import com.spbt2.book.springboot.web.dto.PostsListResponseDto;
 import com.spbt2.book.springboot.web.dto.PostsResponseDto;
 import com.spbt2.book.springboot.web.dto.PostsSaveRequestDto;
 import com.spbt2.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
