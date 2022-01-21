f1e5dc8e454ded42a0c6c5487879ef80acc8faef
hongbi-lee <foxgirin950@gmail.com>
2022-01-21 22:12:47 +0900

    add bookmark modify function

--- bookmarks/bookmark/views.py
+++ bookmarks/bookmark/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.detail import DetailView
 
 from .models import Bookmark
 from django.urls import reverse_lazy
@@ -14,3 +15,13 @@ class BookmarkCreateView(CreateView):
     fields = ['site_name', 'url']   # fields : 어떤 필드들을 입력받을 것인지
     success_url = reverse_lazy('list')  # success_url : 글쓰기를 완료하고 이동할 페이지
     template_name_suffix = '_create'    # 사용할 템플릿의 접미사만 변경하는 설정값
+
+
+class BookmarkDetailView(DetailView):
+    model = Bookmark
+
+
+class BookmarkUpdateView(UpdateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    template_name_suffix = '_update'
