a1cd982004435f30b56a5ae16aa1e6fee13224ea
hongbi-lee <foxgirin950@gmail.com>
2022-03-02 19:56:59 +0900

    add login/logout function

--- dstagram/accounts/urls.py
+++ dstagram/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('photo.urls')),
+    path('login/', auth_view.LoginView.as_view(), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
